c79d4fa6193690db4ea4bd1a9248477514036cf1
r.evbatyrov <r.evbatyrov@codesys.com>
2024-02-26 08:51:38 +0100

    Object and Factory have been implmented

---
+++ WorkshopObject/Factory.cs
@@ -0,0 +1,66 @@
+﻿using _3S.CoDeSys.ApplicationObject;
+using _3S.CoDeSys.Core.Components;
+using _3S.CoDeSys.Core.Objects;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkshopObject
+{
+    [TypeGuid("{7061AFE9-40F1-4131-94AD-94A70FC015E1}")]
+    public class WorkshopObjectFactory : IObjectFactory
+    {
+        private readonly WorkshopObjectWizard _wizard = new WorkshopObjectWizard();
+
+        public Guid Namespace => WorkshopObject.POU_NAMESPACE;
+
+        public string Name => Resource.WorkshopObjectName;
+
+        public string Description => Resource.WorkshopObjectDescription;
+
+        public Icon SmallIcon
+        {
+            get 
+            {
+                // return Resource.WorkshopObjectSmallIcon not recommended, because the internal implementation will cause handle leak
+                return Icons.WorkshopIcon; // a better implementation, because the static Icons class is used
+            }
+        }
+
+        public Icon LargeIcon => SmallIcon;
+
+        public Control WizardPage => _wizard;
+
+        public Control ObjectNameControl => _wizard.textBox1;
+
+        public string ObjectBaseName => "WORK_SHOP";
+
+        public Type ObjectType => typeof(WorkshopObject);
+
+        public Type[] EmbeddedObjectTypes => null;
+
+        public bool AcceptsParentObject(IObject parentObject)
+        {
+            return parentObject is IApplicationObject;
+        }
+
+        public IObject Create()
+        {
+            return new WorkshopObject();
+        }
+
+        public IObject Create(string[] stBatchArguments)
+        {
+            return Create();
+        }
+
+        public void ObjectCreated(int nProjectHandle, Guid guidObject)
+        {
+            // do nothing here
+        }
+    }
+}
--- WorkshopObject/WorkshopObject.cs
+++ WorkshopObject/WorkshopObject.cs
@@ -1,6 +1,7 @@
 ﻿using _3S.CoDeSys.ApplicationObject;
 using _3S.CoDeSys.Core.Components;
 using _3S.CoDeSys.Core.Objects;
+using _3S.CoDeSys.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +13,11 @@ namespace WorkshopObject
     [TypeGuid("{BB3037F3-770B-48F3-86CF-79B50430B433}")]
     public class WorkshopObject : GenericObject2, IWorkshopObject
     {
-        private readonly Guid POU_NAMESPACE = new Guid("{E5B60C93-5445-4e40-ADA9-CD9C005549B4}");
+        public static readonly Guid POU_NAMESPACE = new Guid("{E5B60C93-5445-4e40-ADA9-CD9C005549B4}");
         private string _stText;
-
+        IUniqueIdGenerator _idGen = new LocalIdGenerator();
         private IMetaObject _metaObject;
+
 
         public string Text 
         {
@@ -65,20 +67,21 @@ namespace WorkshopObject
 
         public string GetContentString(ref long nPosition, ref int nLength, bool bWord)
         {
-            throw new NotImplementedException();
+            return "TODO"; // something like "request, and to save its state persistently." (content)
         }
 
         public string GetPositionText(long nPosition)
         {
-            return "TODO";
+            return "TODO"; // should return something like "page 40, line 2" (position as text)
         }
 
         public void HandleRenamed()
         {
-            throw new NotImplementedException();
         }
 
-        public IUniqueIdGenerator UniqueIdGenerator => throw new NotImplementedException();
+        public IUniqueIdGenerator UniqueIdGenerator => _idGen;
+
+        class LocalIdGenerator : DefaultUniqueIdGenerator, IUniqueIdGenerator { }
     }
 
 }
